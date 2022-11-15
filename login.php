a3d8e6d7b9ed82ed57936731d580a1773df533d2
Bino <snowbin82@gmail.com>
2022-11-15 09:37:08 +0100

    modif responsive

--- login.php
+++ login.php
@@ -4,11 +4,12 @@
      <a href="./index.php">Retour à l'accueil</a><br>
 <fieldset>
     
-    <form action="./log_exec.php" method="POST">
-        <label for="username">Nom d'Utilisateur</label>
-        <input type="text" name="username" id="usname">
-        <label for="user_pass">Mot de Passe</label>
-        <input type="password"
-            name="user_pass"><button type="submit" name="submit">Connection</button></form>
-</fieldset><?php include "./include/footer.php";
+    <form action="./log_exec.php" id="formulaire" method="POST">
+        <label id="usname" for="username"> Nom d'utilisateur </label>
+        <input id="utilisateur" type="text" name="username"><br>
+        <label for="user_pass">Mot de passe</label>
+        <input type="password" name="user_pass" id="mdp"><br>
+        <button type="submit" name="submit" id="co">Connexion</button></form>
+</fieldset>
+<?php include "./include/footer.php";
 ?>
