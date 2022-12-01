031841d6fdf288c9a537637b6fd22fe606056c28
Bino <snowbin82@gmail.com>
2022-12-01 16:33:24 +0100

    last changes

--- gest_membre.php
+++ gest_membre.php
@@ -27,8 +27,8 @@ while($data =$req->fetch()){
   echo"
     <article class='membre'>
       <p>$data->id</p>
-      <p>".mb_strimwidth($data->username, 0, 10, '...')."</p>
-      <p>".mb_strimwidth($data->nom, 0, 10, '...')."</p>
+      <p>".mb_strimwidth($data->username, 0, 9, '...')."</p>
+      <p>".mb_strimwidth($data->nom, 0, 9, '...')."</p>
       <p>$data->prenom</p>
       <p>
         <a class='btn btn-link' href='./update_membre.php?id=$data->id'><i class='fa-solid fa-pen-to-square'></i> </a>
