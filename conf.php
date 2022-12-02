5f234cb123daaac790471bb5a86f76c357907bd2
Bino <snowbin82@gmail.com>
2022-12-02 09:29:54 +0100

    orthographe

--- conf.php
+++ conf.php
@@ -7,7 +7,7 @@ include './include/header.php';
 
 if(isset($_SESSION['username'])){
     echo "<h3 class='username'> Bonjour &nbsp;" .$_SESSION['username']." .</h3>
-    <h2>Félicitations,vous faites parti des membres de Bio Ensemble </h2>" ;  
+    <h2>Félicitations,vous faites partie des membres de Bio Ensemble </h2>" ;  
 }    
 
 
